b7c674eb16f36a672c68ded3b83f178c534923a0
GustavoDeSouzaFonseca <rm94067@fiap.com.br>
2023-11-04 20:59:15 -0300

    [:construction: :lipstick:] Adicionado modal de delete, tela de cadastro e estilizacao de cadastro

--- src/pages/clientes/CadastraEmpresa.jsx
+++ src/pages/clientes/CadastraEmpresa.jsx
@@ -1,7 +1,127 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from '../../components/Header';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function CadastraEmpresa() {
-  return <div />;
+  const [nome, setNome] = useState('');
+  const [senha, setSenha] = useState('');
+  const [empresa, setEmpresa] = useState('');
+  const [cnpj, setCnpj] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [numero, setNumero] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleCriaEmpresa = () => {
+    const body = {
+      ds_nome: nome,
+      ds_senha: senha,
+      nm_empresa: empresa,
+      nr_cnpj: cnpj,
+      ds_endereco: endereco,
+      nr_numero: numero,
+      nr_telefone: telefone,
+      ds_email: email
+    }
+
+    try {
+      axios.post("http://localhost:8080/clientes", body)
+      .then(async (response) => {
+        if(response.status === 200) {
+          await toast.success('Empresa criada com sucesso!', {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+
+          setNome('');
+          setSenha('');
+          setEmpresa('');
+          setCnpj('');
+          setEndereco('');
+          setNumero('');
+          setTelefone('');
+          setEmail('');
+
+          return;
+        } else {
+          toast.error(response.statusText, {
+            position: "bottom-left",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+          return;
+        }
+      })
+      .catch((err) => {
+         toast.error("Erro interno do servidor", {
+          position: "bottom-left",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: false,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+        return;
+      })
+    } catch (error) {
+      toast.error(error, {
+        position: "bottom-left",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  }
+
+  return (
+    <div className="flex flex-col h-screen">
+      <Header/>
+      <section className='flex-1 bg-purple-200 flex justify-center'>
+
+        <div className='w-5/12 h-4/6 mt-20 bg-gradient-to-r from-purple-600/30
+          via-indigo-500/30 to-cyan-400/30 rounded-3xl flex flex-col items-center'>
+            <h2 className='text-white font-mono text-xl mt-10'>Cadastra Empresa</h2>
+
+            <div className='w-5/6 mt-5'>
+              <div className='flex flex-row justify-between'>
+                <input  className='bg-zinc-600/50 w-6/12 mr-1 p-1 h-10 rounded-md flex' type='text' placeholder='nome' value={nome} onChange={(e) => {setNome(e.target.value)}} />
+                <input className='bg-zinc-600/50 w-6/12 ml-1 p-1 h-10 rounded-md flex' type='password' placeholder='senha' value={senha} onChange={(e) => {setSenha(e.target.value)}}/>
+              </div>
+              <input className='bg-zinc-600/50 w-full p-1 h-10 mt-1 rounded-md flex' type='text' placeholder='empresa' value={empresa} onChange={(e) => {setEmpresa(e.target.value)}}/>
+              <input className='bg-zinc-600/50 w-full p-1 h-10 mt-1 rounded-md flex' type='text' placeholder='cnpj' value={cnpj} onChange={(e) => {setCnpj(e.target.value)}}/>
+              <div className='flex flex-row justify-between w-full'>
+                <input className='bg-zinc-600/50 w-8/12 mr-1 p-1 h-10 mt-1 rounded-md flex' type='text' placeholder='endereco' value={endereco} onChange={(e) => {setEndereco(e.target.value)}}/>
+                <input className='bg-zinc-600/50 w-4/12 ml-1 p-1 h-10 mt-1 rounded-md flex' type='text' placeholder='numero' value={numero} onChange={(e) => {setNumero(e.target.value)}}/>
+              </div>
+            <input className='bg-zinc-600/50 w-full p-1 h-10 mt-1 rounded-md flex' type='text' placeholder='email' value={email} onChange={(e) => {setEmail(e.target.value)}}/>
+            <div className='w-full flex flex-row justify-between'>
+              <input className='bg-zinc-600/50 w-3/6 p-1 mr-1 h-10 mt-1 rounded-md flex' type='text' placeholder='telefone' value={telefone} onChange={(e) => {setTelefone(e.target.value)}}/>
+              <button className='bg-violet-500 text-white w-3/6 ml-1 h-10 mt-1 items-center justify-center rounded-md flex' onClick={handleCriaEmpresa}>Criar Empresa</button>
+            </div>
+            </div>
+        </div>
+      </section>
+    </div>  
+  );
 }
 
 export default CadastraEmpresa;
--- src/pages/clientes/Clientes.jsx
+++ src/pages/clientes/Clientes.jsx
@@ -9,6 +9,9 @@ function Clientes() {
   const BASE_URL = "http://localhost:8080"
 
   const [clientes, setClientes] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [clienteToDelete, setClienteToDelete] = useState(null);
+
 
   useEffect(() => {
     async function fetchClientes() {
@@ -22,6 +25,27 @@ function Clientes() {
     fetchClientes();
   }, []);
 
+  const openModal = (clienteId) => {
+    setClienteToDelete(clienteId);
+    setShowModal(true);
+  }
+  
+  const closeModal = () => {
+    setClienteToDelete(null);
+    setShowModal(false);
+  }
+  
+  const handleDelete = async (clienteId) => {
+    try {
+      await axios.delete(`${BASE_URL}/clientes/${clienteId}`);
+      setClientes(clientes.filter(cliente => cliente.id !== clienteId));
+      closeModal();
+    } catch (error) {
+      console.error('Erro ao excluir cliente:', error);
+      closeModal();
+    }
+  }
+  
 
   return (
     <div className="flex flex-col h-screen">
@@ -30,28 +54,42 @@ function Clientes() {
         <div className='w-5/12 h-4/6 mt-20 bg-gradient-to-r from-purple-600/30
           via-indigo-500/30 to-cyan-400/30 rounded-3xl flex flex-col items-center'>
           <h2 className='text-white font-mono text-xl mt-10'>Clientes</h2>
-          <ul className='w-10/12 h-5/6 mt-7'>
-            <div className='bg-zinc-600/30 w-full h-8 rounded-md flex mt-15'>
+          <div className='bg-zinc-600/50 w-5/6 h-10 rounded-md flex mt-15'>
               <span className='ml-5 w-8 text-white'></span>
-              <span className='w-24 text-white'>Nome</span>
-              <span className='w-36 text-white'>Empresa</span>
+              <span className='w-24 text-white ml-3'>Nome</span>
+              <span className='w-36 text-white ml-3'>Empresa</span>
             </div>
+          <ul className='w-10/12 h-5/6 mt-7 ulClientes overflow-y-auto'>
+            
             {clientes.map(cliente => (
               <div className='mt-5'>
                 <li className='bg-zinc-600/30 w-full h-8 rounded-md flex mt-15' key={cliente.id}>
                   <span className='ml-5 w-8 text-white'>{cliente.id}</span>
-                  <span className='w-24 text-white'>{cliente.ds_nome}</span>
-                  <span className='w-36 text-white'>{cliente.nm_empresa}</span>
+                  <span className='w-24 text-white truncate ml-3'>{cliente.ds_nome}</span>
+                  <span className='w-36 text-white truncate ml-3'>{cliente.nm_empresa}</span>
                   <div className="flex ml-auto items-center mr-3">
-                    <FontAwesomeIcon icon={faEdit} className="text-white mr-2 cursor-pointer" />
-                    <FontAwesomeIcon icon={faTrash} className="text-white cursor-pointer" />
+                    <FontAwesomeIcon icon={faEdit} className="text-white mr-2 cursor-pointer"/>
+                    <FontAwesomeIcon icon={faTrash} className="text-white cursor-pointer" onClick={() => openModal(cliente.id)}/>
                   </div>
                 </li>
               </div>
             ))}
+
+            {showModal && (
+              <div className="fixed inset-0 flex items-center justify-center z-50">
+                <div className="bg-white p-5 rounded-lg shadow-lg">
+                  <p className="mb-3">Tem certeza que deseja excluir este cliente?</p>
+                  <div className="flex justify-end">
+                    <button className="bg-violet-500 text-white px-4 py-2 rounded mr-3" onClick={() => handleDelete(clienteToDelete)}>Excluir</button>
+                    <button className="bg-gray-300 text-black px-4 py-2 rounded" onClick={closeModal}>Cancelar</button>
+                  </div>
+                </div>
+              </div>
+            )}
           </ul>
+
           <Link to={"/empresa/cadastrar"}>
-            <div className='w-56 flex items-center justify-center h-[35px] rounded-2xl bg-zinc-600 mb-5'>
+            <div className='w-56 flex items-center justify-center h-[35px] rounded-2xl bg-zinc-600 mb-5 mt-3'>
               <span className='text-white'>Criar uma nova empresa</span>
             </div>
           </Link>
